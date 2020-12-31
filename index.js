82e1c82c53ddf99ed1feb965ef66a3aa23346bb4
SeaBassLab <70863556+SeaBassLab@users.noreply.github.com>
2020-12-31 07:29:21 -0300

    fix the button selector

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Media Player
 
 const audio = document.querySelector('audio')
-const boton = document.querySelector('button')
+const boton = document.getElementById('button')
 const input = document.getElementById('volumen')
 
 const player = new MediaPlayer({ el: audio })
